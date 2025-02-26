f6ec33427c111cc41ff416d14ea7e321f719d7fc
hahuyhoang411 <hahuyhoanghhh41@gmail.com>
2025-02-26 18:10:15 +0100

    fix prompt

--- lm_eval/tasks/leaderboard-french/mmlu_pro/utils.py
+++ lm_eval/tasks/leaderboard-french/mmlu_pro/utils.py
@@ -7,7 +7,7 @@ def doc_to_text(doc):
     for i in range(len(options)):
         doc_to_text += f"{string.ascii_uppercase[i]}. {options[i]}\n"
     
-    doc_to_text += "Réponse:"
+    doc_to_text += "Format your final answer in \\boxed{([A-D])}. Réponse:"
     return doc_to_text
 
 def doc_to_choice(doc):

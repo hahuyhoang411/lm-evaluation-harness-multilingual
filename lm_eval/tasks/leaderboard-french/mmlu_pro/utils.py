a0462f0fb6a7fd1259174b42ebc90934a841cc0c
hahuyhoang411 <hahuyhoanghhh41@gmail.com>
2025-02-26 13:10:57 +0100

    fix mmlu to box

--- lm_eval/tasks/leaderboard-french/mmlu_pro/utils.py
+++ lm_eval/tasks/leaderboard-french/mmlu_pro/utils.py
@@ -13,13 +13,3 @@ def doc_to_text(doc):
 def doc_to_choice(doc):
     options = [doc['A'], doc['B'], doc['C'], doc['D']]
     return [string.ascii_uppercase[i] for i in range(len(options))]
-
-def extract_answer_from_box(response):
-    # First, try extracting content inside box
-    box_match = re.search(r"\\boxed\{(.*?)\}", response, re.DOTALL)
-    if box_match:
-        return box_match.group(1).strip()
-
-    # If no box is found, fall back to extracting the first valid answer choice
-    match = re.search(r'\b[A-D]\b', response)
-    return match.group(0) if match else None

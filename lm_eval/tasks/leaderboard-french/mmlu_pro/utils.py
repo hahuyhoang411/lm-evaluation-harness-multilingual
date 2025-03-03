09e96881619ec3137e6e31cdb58ecbc736646f33
hahuyhoang411 <hahuyhoanghhh41@gmail.com>
2025-03-03 22:20:08 +0100

    fix prompt mmlu

--- lm_eval/tasks/leaderboard-french/mmlu_pro/utils.py
+++ lm_eval/tasks/leaderboard-french/mmlu_pro/utils.py
@@ -2,12 +2,15 @@ import string
 import re
 
 def doc_to_text(doc):
-    doc_to_text = f"{doc['Question']}\n"
+    doc_to_text = "Répondez à la question à choix multiples suivante.\n\n"
+    doc_to_text += f"Question:\n{doc['Question']}\n\n"
+    doc_to_text += "Options:\n"
+    
     options = [doc['A'], doc['B'], doc['C'], doc['D']]
     for i in range(len(options)):
         doc_to_text += f"{string.ascii_uppercase[i]}. {options[i]}\n"
-    
-    doc_to_text += "Formatez votre réponse finale en \\boxed{([A-D])}. Résponse:\n"
+
+    doc_to_text += "\nFormatez votre réponse finale en \\boxed{lettres} où lettres est l'une des lettres A, B, C ou D.\n"
     return doc_to_text
 
 def doc_to_choice(doc):
